b04bb6f3175c66358310bd58793ad156260241bf
Polina Suboch <palina.subach@gmail.com>
2023-03-22 16:13:57 +0400

    Initialize project

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+from app.home.views import router as home_router
+
+app = FastAPI(
+    title="FastAPI GraphQL",
+    description="Welcome to FastAPI GraphQL project documentation!",
+    root_path="/",
+    docs_url='/docs',
+    openapi_url="/docs/openapi.json",
+    redoc_url="/redoc"
+)
+
+
+app.include_router(home_router, prefix="/home", tags=["home"])
+
